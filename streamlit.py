cf98c4b7b578cfe8f930c35afdd0b7ec7189aa4c
benagastov <106045877+benagastov@users.noreply.github.com>
2022-12-26 09:29:16 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -86,7 +86,7 @@ class PIDController:
 
 
 with tab3:
-    col1, col2 = st.columns([3, 1])
+    col1, col2 = st.columns([1, 3])
     with col1:
         Kp =  st.slider("Kp", min_value=0.0, max_value=0.4, value=0.1, step=0.05)
         Ki =  st.slider("Ki", min_value=0.0, max_value=0.5, value=0.1, step=0.01)
