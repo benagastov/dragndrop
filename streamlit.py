96c798203c25698669c0f9284c8d962a9dd9da46
benagastov <106045877+benagastov@users.noreply.github.com>
2022-12-26 08:15:54 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from IPython import display
-display.set_matplotlib_formats('svg')
 
 #define constants
 alpha, beta = 1, 35
@@ -12,6 +11,7 @@ def next_temp(u, T, dt):
     return T+alpha*(T_ambient-T)*dt + beta * u *dt
 
 def simulate_temp(controller, num_steps=20):
+    fig = plt.figure() 
     dt = 0.1 # Every time interval dt we set a new control value
     T = T_start
     T_list = [T]
@@ -27,6 +27,7 @@ def simulate_temp(controller, num_steps=20):
     plt.plot(time, np.repeat(T_desired, num_steps+1), ls="--")
     plt.plot(time, T_list)
     plt.xlabel("time"); plt.ylabel("Temperature");
+    st.pyplot(fig)
 
 class SillyController:
     def get_control(self,T,dt):
